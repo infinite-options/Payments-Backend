32a08eac6753eedbbe60f6c3ec6a691bfbedd58f
pmarathay <pmarathay@infiniteoptions.com>
2021-04-22 23:24:25 -0700

    created off session charge and refunds

--- payments_api.py
+++ payments_api.py
@@ -198,6 +198,7 @@ class createPaymentIntentOnly(Resource):
         return client_secret
 
 
+# STEP 1,2,3 COMBINED: Create a Payment Intent
 class createPaymentIntent(Resource):
     def post(self):
 
@@ -231,12 +232,155 @@ class createPaymentIntent(Resource):
         return paymentIntent
 
 
+# Step 4: Payment method with card and billing details entirely on the Front End
+
+# Step 5: Submit Payment to Stripe is handled entirely on the Front End
+
+
+# Step 6: Submit Off Session payment to Strip
+class createOffSessionPaymentIntent(Resource):
+    def post(self):
+
+        data = request.get_json(force=True)
+        print("\ndata: ", data)
+        customer_uid = data["customer_uid"]
+        businessId = data["business_code"]
+        charge_amount = int(round(float(data["payment_summary"]["total"]) * 100))
+        print("customer: ", customer_uid)
+        print("business: ", businessId)
+        print("amount: ", charge_amount)
+
+        print("\nIn Step 1")
+        keys = getCorrectKeys.post(self, businessId)
+        print("stripe PUBLISHABLE_KEY: ", keys["PUBLISHABLE_KEY"])
+        print("stripe SECRET_KEY: ", keys["SECRET_KEY"])
+        stripe.api_key = keys["SECRET_KEY"]
+        print("stripe api key: ", stripe.api_key)
+        stripe.api_version = None
+
+        print("\nIn Step 6")
+        intent = stripe.PaymentIntent.create(
+            amount=charge_amount,
+            currency="usd",
+            # Verify your integration in this guide by including this parameter
+            # metadata={'integration_check': 'accept_a_payment'},
+            # customer="cus_JKUnLFjlbjW2PG",
+            customer=customer_uid,
+            # customer='{{CUSTOMER_ID}}',
+            # payment_method="pm_1IhpoELMju5RPMEvq6B92VsG",
+            # payment_method='{{PAYMENT_METHOD_ID}}',
+            payment_method=stripe.PaymentMethod.list(
+                customer=data["customer_uid"],
+                type="card",
+            )
+            .data[0]
+            .id,
+            off_session=True,
+            confirm=True,
+        )
+
+        print("\nPayment Intent: ", intent.id)
+        print("Payment Intent with Secret: ", intent.client_secret)
+        print("Charge ID: ", intent.charges.data[0].id)
+        print("\nintent: ", intent)
+        chargeId = intent.charges.data[0].id
+        client_secret = intent.client_secret
+        # return {"secret": client_secret}
+        return chargeId
+
+
+# Retrieve Stripe Info - Utilities
+class retrieveStripeCharge(Resource):
+    def post(self):
+
+        data = request.get_json(force=True)
+        print("\ndata: ", data)
+        customer_uid = data["customer_uid"]
+        businessId = data["business_code"]
+        charge_amount = int(round(float(data["payment_summary"]["total"]) * 100))
+        print("customer: ", customer_uid)
+        print("business: ", businessId)
+        print("amount: ", charge_amount)
+
+        print("\nIn Step 1")
+        keys = getCorrectKeys.post(self, businessId)
+        print("stripe PUBLISHABLE_KEY: ", keys["PUBLISHABLE_KEY"])
+        print("stripe SECRET_KEY: ", keys["SECRET_KEY"])
+        stripe.api_key = keys["SECRET_KEY"]
+        print("stripe api key: ", stripe.api_key)
+        stripe.api_version = None
+
+        # # Retrieve Charge ID (Typically from Mobile) - works
+        # # Charge ID has a SUBSET of Payment Intent
+        # print("\nRetrieve Charge ID Info")
+        # retrieveInfo = stripe.Charge.retrieve("ch_1Ii1DPGQZnKn7zmSEmC80Hx7")
+        # print("Stripe Charge ID Info: ", retrieveInfo)
+
+        # Retrieve Payment Intent (Typically from Web or Postman) - -works
+        # Payment Intent has Everything
+        print("\nRetrieve Payment Intent Info")
+        # retrieveInfo = stripe.PaymentIntent.retrieve("pi_1IjEA3LMju5RPMEvKBUwxxhJ") - for M4ME
+        # retrieveInfo = stripe.PaymentIntent.retrieve("pi_1Ii1DOGQZnKn7zmSWXJHNS8F")
+        retrieveInfo = stripe.PaymentIntent.retrieve("pi_1IjI2VGQZnKn7zmSYhAlSEAY")
+        print("\nPayment Intent: ", retrieveInfo.id)
+        print("Payment Intent with Secret: ", retrieveInfo.client_secret)
+        print("Charge ID: ", retrieveInfo.charges.data[0].id)
+        print("\nStripe Payment Intent Info: ", retrieveInfo)
+
+        # # Retrieve Payment Method - works
+        # # Payment Method has customer ID, Name, Last 4 cc, Address
+        # print("\nRetrieve Payment Method Info")
+        # retrieveInfo = stripe.PaymentMethod.retrieve("pm_1Ii1DOGQZnKn7zmSer9GLvPi")
+        # print("Stripe Payment Method Info: ", retrieveInfo)
+
+        # # Retrieve All Customers - works
+        # print("\nRetrieve All Customers per limit")
+        # retrieveInfo = stripe.Customer.list(limit=30)
+        # print("Stripe All Customers Info: ", retrieveInfo)
+
+        return retrieveInfo
+
+
+# Process a Refund
+class refund(Resource):
+    def post(self):
+
+        data = request.get_json(force=True)
+        print("\ndata: ", data)
+        customer_uid = data["customer_uid"]
+        businessId = data["business_code"]
+        # charge_amount = int(round(float(data["payment_summary"]["total"]) * 100))
+        print("customer: ", customer_uid)
+        print("business: ", businessId)
+        # print("amount: ", charge_amount)
+
+        print("\nIn Step 1")
+        keys = getCorrectKeys.post(self, businessId)
+        print("stripe PUBLISHABLE_KEY: ", keys["PUBLISHABLE_KEY"])
+        print("stripe SECRET_KEY: ", keys["SECRET_KEY"])
+        stripe.api_key = keys["SECRET_KEY"]
+        print("stripe api key: ", stripe.api_key)
+        stripe.api_version = None
+
+        # Process a Refund
+        print("\nProcess a Refund")
+        retrieveInfo = stripe.Refund.create(
+            charge="ch_1IjICJGQZnKn7zmSzdTa8cBC", amount=2000
+        )
+        print("Stripe Refund Info: ", retrieveInfo)
+
+        return retrieveInfo
+
+
 # Define API routes
 # NEW BASE URL:  https://huo8rhh76i.execute-api.us-west-1.amazonaws.com/dev/
 api.add_resource(getCorrectKeys, "/api/v2/getCorrectKeys/<string:businessId>")
 api.add_resource(createNewCustomer, "/api/v2/createNewCustomer/<string:customer_uid>")
 # api.add_resource(createCustomerOnly, "/api/v2/createCustomerOnly<string:customer_uid>")
 api.add_resource(createPaymentIntent, "/api/v2/createPaymentIntent")
+api.add_resource(retrieveStripeCharge, "/api/v2/retrieveStripeCharge")
+api.add_resource(createOffSessionPaymentIntent, "/api/v2/createOffSessionPaymentIntent")
+api.add_resource(refund, "/api/v2/refund")
 
 
 if __name__ == "__main__":
